8d38329dcb651b3f7ecd43ebd1a2e73f2972745b
MilnMiln <miln@gmx.com>
2019-06-06 23:13:54 -0700

    Updated lists.py. Added examples and output formatting.

--- lists.py
+++ lists.py
@@ -5,27 +5,26 @@ myList.append(1)
 myList.append(2)
 myList.append(3)
 
-print(myList[0])
-print(myList[1])
-print(myList[2])
+print("Element 0 of",myList,"is:",myList[0])
 
-myOtherList = [4,5,6,7]
+myOtherList = [5,6,7]
 
-for x in myOtherList:
+for x in myOtherList:		
     print(x)
 
 numbers = []
 strings = []
 names = ["John", "Eric", "Jessica"]
 
-numbers.append([1,2,3]) # Doesn't work. Try concatenation sometime?
+#numbers.append([1,2,3]) # Doesn't work.
+numbers.extend(myList)
 numbers.append(4)
-numbers.append(5)
-numbers.append(6)
+numbers.extend(myOtherList)
+
 strings.append("hello")
 strings.append("world")
 second_name = names[1]
 
 print(numbers)
 print(strings)
-print("2nd name is %s." % second_name)
+print("2nd name is: %s" % second_name)
